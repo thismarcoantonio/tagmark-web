781f33aed04353d04356aa9545f3efb2a5011ffe
thismarcoantonio <thismarcoantonio@gmail.com>
2025-09-20 22:39:34 -0700

    Add MainHeader component

---
+++ src/components/MainHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <header class="flex items-center p-4">
+    <h1 class="text-xl font-bold mr-4"><span class="text-green-700">Tag</span>Mark</h1>
+    <input
+      class="border border-neutral-200 bg-white rounded-full px-4 py-2 w-1/2"
+      placeholder="Search..."
+    />
+    <main-button variant="secondary" class="ml-auto mr-3">New tag</main-button>
+    <main-button>New bookmark</main-button>
+  </header>
+</template>
+
+<script lang="ts" setup>
+import MainButton from '@/components/MainButton.vue';
+</script>
