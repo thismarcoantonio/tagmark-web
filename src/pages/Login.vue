d43a310736887198bd8bbc53aedf11391ca9e759
thismarcoantonio <thismarcoantonio@gmail.com>
2025-09-20 22:40:02 -0700

    Add Login component

---
+++ src/pages/Login.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>TagMark</h1>
+  <p>Save your bookmarks using tags to facilitate finding it later!</p>
+  <main-card>
+    <text-field label="Email" name="email" />
+    <text-field label="Password" name="password" type="password" />
+  </main-card>
+</template>
+
+<script lang="ts" setup>
+import MainCard from '@/components/MainCard.vue';
+import TextField from '@/components/TextField.vue';
+</script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import Homepage from '@/pages/Homepage.vue';
+import Login from '@/pages/Login.vue';
+
+enum Routes {
+  Login = 'login',
+  Homepage = 'homepage',
+}
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    { path: '/', name: Routes.Homepage, component: Homepage },
+    { path: '/login', name: Routes.Login, component: Login },
+  ],
 });
 
 export default router;
