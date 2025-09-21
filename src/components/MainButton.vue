60f9356c7731c5416c964bd5c6cb643ef15e6d03
thismarcoantonio <thismarcoantonio@gmail.com>
2025-09-20 22:39:21 -0700

    Add MainButton component

---
+++ src/components/MainButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <button
+    class="py-3 px-4 rounded-full text-sm font-semibold cursor-pointer"
+    :class="{
+      'bg-green-700 hover:bg-green-800 text-white': variant === 'primary',
+      'bg-slate-300 hover:bg-slate-400': variant === 'secondary',
+    }"
+  >
+    <slot />
+  </button>
+</template>
+
+<script lang="ts" setup>
+interface Props {
+  variant?: 'primary' | 'secondary' | 'text';
+}
+
+withDefaults(defineProps<Props>(), {
+  variant: 'primary',
+});
+</script>
