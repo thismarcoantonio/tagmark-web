96e5b52c695cdaaa2cad810368938c5acee7dcbc
thismarcoantonio <thismarcoantonio@gmail.com>
2025-09-20 22:39:56 -0700

    Add TextField component

---
+++ src/components/TextField.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <label class="block" :for="name">
+      {{ label }}
+    </label>
+    <input
+      class="border border-neutral-300 rounded-md"
+      :name="name"
+      :id="name"
+      :type="type || 'text'"
+    />
+  </div>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  name: string;
+  label: string;
+  type?: string;
+}>();
+</script>
