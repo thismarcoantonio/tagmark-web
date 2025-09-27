e292ea62c6e6a10fd5a42cf6078cc346d4fed476
thismarcoantonio <thismarcoantonio@gmail.com>
2025-09-27 01:41:20 -0700

    Update Header and Button components

--- src/components/MainButton.vue
+++ src/components/MainButton.vue
@@ -2,11 +2,12 @@
   <component
     :is="to ? 'router-link' : 'button'"
     :to="to"
-    class="cursor-pointer rounded-xl border border-b-4 px-4 py-3 font-semibold transition-colors"
+    class="cursor-pointer border border-b-4 px-4 py-3 font-semibold transition-colors"
     :class="{
       'border-gray-600 bg-green-600 text-white hover:bg-green-700': variant === 'primary',
-      'bg-slate-300 hover:bg-slate-400': variant === 'secondary',
+      'bg-white hover:bg-gray-100': variant === 'secondary',
       'rounded-full': rounded,
+      'rounded-xl': !rounded,
     }"
   >
     <slot />
--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,18 +1,25 @@
 <template>
-  <header class="flex items-center p-4">
-    <h1 class="mr-4 text-xl font-bold"><span class="text-green-700">Tag</span>Mark</h1>
-    <input
-      class="w-1/2 rounded-full border border-neutral-200 bg-white px-4 py-2"
-      placeholder="Search..."
-    />
-    <main-button variant="secondary" class="mr-3 ml-auto" :to="{ path: Routes.MyTags }">
-      My tags
-    </main-button>
-    <main-button>New bookmark</main-button>
+  <header class="flex items-center justify-between p-4">
+    <main-title />
+    <div class="relative flex w-1/2">
+      <input
+        class="w-full rounded-full border border-b-4 border-neutral-200 bg-white px-4 py-3 outline-0"
+        placeholder="Search..."
+      />
+      <search-icon class="absolute top-[13px] right-5" :size="22" />
+    </div>
+    <div>
+      <main-button rounded variant="secondary" class="mr-3" :to="{ path: Routes.MyTags }">
+        My tags
+      </main-button>
+      <main-button rounded>New bookmark</main-button>
+    </div>
   </header>
 </template>
 
 <script lang="ts" setup>
-import MainButton from '@/components/MainButton.vue';
+import { SearchIcon } from 'lucide-vue-next';
 import { Routes } from '@/router';
+import MainTitle from '@/components/MainTitle.vue';
+import MainButton from '@/components/MainButton.vue';
 </script>
