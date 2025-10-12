427169fc8c8fe63f2e291d208b1b36261fee0ac5
thismarcoantonio <thismarcoantonio@gmail.com>
2025-10-11 21:11:43 -0700

    Add vee-validate

--- src/components/TextField.vue
+++ src/components/TextField.vue
@@ -3,16 +3,19 @@
     <label class="block text-gray-800" :for="name">
       {{ label }}
     </label>
-    <input
+    <v-field
       class="w-full rounded-xl border border-b-2 border-gray-300 px-4 py-2 outline-0 focus:border-green-600"
       :name="name"
       :id="name"
       :type="type || 'text'"
     />
+    <v-error-message :name="name" class="text-sm text-red-500" />
   </div>
 </template>
 
 <script lang="ts" setup>
+import { Field as VField, ErrorMessage as VErrorMessage } from 'vee-validate';
+
 defineProps<{
   name: string;
   label: string;
