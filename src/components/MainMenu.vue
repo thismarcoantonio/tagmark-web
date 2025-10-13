266d64d85bfdc649ea9682093bfa03389ab8c37c
thismarcoantonio <thismarcoantonio@gmail.com>
2025-10-12 20:46:57 -0700

    Add MainMenu component using headless-ui

---
+++ src/components/MainMenu.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-menu as="div" class="relative inline-block text-left">
+    <v-menu-button>
+      <slot name="activator" />
+    </v-menu-button>
+
+    <transition
+      enter-active-class="transition duration-100 ease-out"
+      enter-from-class="transform scale-95 opacity-0"
+      enter-to-class="transform scale-100 opacity-100"
+      leave-active-class="transition duration-75 ease-in"
+      leave-from-class="transform scale-100 opacity-100"
+      leave-to-class="transform scale-95 opacity-0"
+    >
+      <v-menu-items
+        class="absolute right-0 mt-2 w-56 origin-top-right rounded-md border border-b-4 border-gray-400 bg-white shadow-md focus:outline-none"
+      >
+        <div class="px-1 py-1">
+          <slot />
+        </div>
+      </v-menu-items>
+    </transition>
+  </v-menu>
+</template>
+
+<script lang="ts" setup>
+import { Menu as VMenu, MenuButton as VMenuButton, MenuItems as VMenuItems } from '@headlessui/vue';
+</script>
---
+++ src/components/MainMenuItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-menu-item v-slot="{ active }">
+    <button
+      class="group flex w-full cursor-pointer items-center rounded-md px-2 py-2 text-sm"
+      :class="{
+        'bg-green-600 text-white': active,
+        'text-gray-900': !active,
+      }"
+    >
+      <div v-if="$slots.prepend" class="mr-2">
+        <slot name="prepend" />
+      </div>
+      <slot />
+    </button>
+  </v-menu-item>
+</template>
+
+<script lang="ts" setup>
+import { MenuItem as VMenuItem } from '@headlessui/vue';
+</script>
